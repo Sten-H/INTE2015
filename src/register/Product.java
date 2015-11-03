3fbffb37a025a6836cb1810dc292c8cb44c1a690
viciousc <sabina.windahl@gmail.com>
2015-11-04 00:56:03 +0100

    PASS testFirstContructor()

--- src/register/Product.java
+++ src/register/Product.java
@@ -1,5 +1,13 @@
 package register;
 
 public class Product {
-
+	public Product(String name, double price){
+	}
+	
+	public String getName(){
+		return "fejkon";
+	}
+	public double getPrice(){
+		return 45;
+	}
 }
