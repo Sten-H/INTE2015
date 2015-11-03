eb2c9904bc187cbbf9d2e96320f53f5088be1f3a
Teodor falk <teodor.falk@hotmail.com>
2015-11-03 12:03:59 +0100

    Lagt till testfil för Discount

---
+++ src/tests/DiscountTest.java
@@ -0,0 +1,14 @@
+package tests;
+import static org.junit.Assert.*;
+import register.Discount;
+import java.util.*;
+
+import org.junit.Test;
+
+public class DiscountTest {
+
+	@Test
+	public void testSimpleConstructor(){
+	}
+	
+}
