829a4bd3e7616dec5b9196023ab8a84f4ba1d728
StenH <s@stenh.com>
2015-11-04 00:40:29 +0100

    Small syntax fix

--- src/register/DiscountManager.java
+++ src/register/DiscountManager.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.*;;
+import com.fasterxml.jackson.databind.*;
 
 
 /**
