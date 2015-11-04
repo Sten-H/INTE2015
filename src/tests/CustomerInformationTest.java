cc81d5bbb460153db4f86a3e724317993f2a4703
viciousc <sabina.windahl@gmail.com>
2015-11-04 23:02:13 +0100

    Added test testFirstConstructor2()

--- src/tests/CustomerInformationTest.java
+++ src/tests/CustomerInformationTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import register.CustomerInformation;
+import register.Product;
 
 public class CustomerInformationTest {
 
@@ -15,6 +16,13 @@ public class CustomerInformationTest {
 		CustomerInformation cI = new CustomerInformation("1234567890", 3);
 		assertEquals("1234567890", cI.getCreditCardNumber());		
 		assertEquals(3, cI.getCustomerNumber(), 0);
+	}
+	
+	@Test
+	public void testFirstConstructor2(){
+		CustomerInformation cI = new CustomerInformation("0987654321", 98);
+		assertEquals("0987654321", cI.getCreditCardNumber());
+		assertEquals(98, cI.getCustomerNumber(), 0);
 	}
 	
 }
