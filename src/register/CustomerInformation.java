b803ac7fb2c8c3767da56ecc13812507f1557c9a
viciousc <sabina.windahl@gmail.com>
2015-11-04 22:57:34 +0100

    PASS testFirstConstructor()

--- src/register/CustomerInformation.java
+++ src/register/CustomerInformation.java
@@ -2,4 +2,17 @@ package register;
 
 public class CustomerInformation {
 
+	public CustomerInformation(String creditCardNumber, int customerNumber){
+		
+	}
+	
+	public String getCreditCardNumber(){
+		return "1234567890";
+	}
+	
+	public int getCustomerNumber(){
+		return 3;
+	}
+	
+	
 }
