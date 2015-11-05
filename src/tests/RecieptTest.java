36168361ad8ce26a4a0e478bf2e130267f409ad0
viciousc <sabina.windahl@gmail.com>
2015-11-05 19:13:19 +0100

    Added test testExceptionNullOrder()

--- src/tests/RecieptTest.java
+++ src/tests/RecieptTest.java
@@ -13,19 +13,30 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class RecieptTest {
-
+	Product p1 = new Product("Fejkon", 33);
+	OrderLine ol1 = new OrderLine(p1, 2);
+	ArrayList<OrderLine> orderLineList = new ArrayList<>();
 
 	//Test constructors
 		@Test
 		public void testSimpleConstructor1(){
-			Product p1 = new Product("Fejkon", 33);
-			OrderLine ol1 = new OrderLine(p1, 2);
-			ArrayList<OrderLine> orderLineList = new ArrayList<>();
 			orderLineList.add(ol1);
 			Order order = new Order(orderLineList);
 			Reciept r = new Reciept(order);
 			assertNotNull(r.getOrder());
 		}
+		
+		
+		//Test exceptions
+		@Rule
+		public ExpectedException expectedException = ExpectedException.none(); 
+		
+		@Test
+		public void testExceptionNullOrder(){
+			expectedException.expect(NullPointerException.class);
+		    expectedException.expectMessage("Order can not be null.");
+			Reciept r = new Reciept(null);
+		}		
 }
 
 
