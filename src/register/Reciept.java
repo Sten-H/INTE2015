92088e0382f22df05d2a212092ab23b5b93eaa51
viciousc <sabina.windahl@gmail.com>
2015-11-05 19:08:12 +0100

    PASS testSimpleConstructor()

--- src/register/Reciept.java
+++ src/register/Reciept.java
@@ -1,7 +1,14 @@
 package register;
 
 public class Reciept {
+	private Order order;
 	
+	public Reciept(Order order){
+		this.order = order;
+	}
 
 	
+	public Order getOrder(){
+		return order;
+	}
 }
