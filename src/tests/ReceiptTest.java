beb583d943e493ce75c36c2aeb944a6836403231
viciousc <sabina.windahl@gmail.com>
2015-11-10 15:48:23 +0100

    Changed the testSimpleContructor1() and added setupmethod for DiscountList. Does not pass the JUnit-test in this commit

--- src/tests/ReceiptTest.java
+++ src/tests/ReceiptTest.java
@@ -3,7 +3,8 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
-import register.Order;
+import register.Discount;
+import register.DiscountPair;
 import register.OrderLine;
 import register.Product;
 import register.Receipt;
@@ -11,6 +12,11 @@ import register.Receipt;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 
 public class ReceiptTest {
 	Product p1 = new Product("Fejkon", 33);
@@ -20,14 +26,40 @@ public class ReceiptTest {
 	OrderLine ol2 = new OrderLine(p2, 5);
 	OrderLine ol3 = new OrderLine(p3, 1);
 	ArrayList<OrderLine> orderLineList = new ArrayList<>();
-
+	ArrayList<Discount> validDiscountList = new ArrayList<>();
+	
+	private ArrayList<Discount> createDiscountList(){
+		Calendar cal;
+	    Date start;
+	    Date end;
+	    ArrayList<Discount> validDList = new ArrayList<>();
+	    cal = new GregorianCalendar();
+	    cal.set(Calendar.YEAR, 2015);
+	    cal.set(Calendar.MONTH, 10);
+	    cal.set(Calendar.DATE, 10);
+	    start = cal.getTime();
+	    cal.set(Calendar.YEAR, 2015);
+	    cal.set(Calendar.MONTH, 11);
+	    cal.set(Calendar.DATE, 11);
+	    end = cal.getTime();
+	    
+	    
+	    ArrayList<DiscountPair> discountPairList = new ArrayList<>();
+		DiscountPair discountPair = new DiscountPair(p3, 2);
+		discountPairList.add(discountPair);
+		Discount d = new Discount(start, end, 2.0, discountPairList);
+		validDList.add(d);
+	     
+		return validDList;
+	}
 	//Test constructors
 	@Test
 	public void testSimpleConstructor1(){
+		
 		orderLineList.add(ol1);
 		orderLineList.add(ol2);
 		orderLineList.add(ol3);
-		Receipt r = new Receipt(orderLineList);
+		Receipt r = new Receipt(orderLineList, createDiscountList());
 		assertNotNull(r.getOrderLineList());
 	}
 	
