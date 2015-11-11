97b2b7d3f628af87f7660384336bd7738421b94d
viciousc <sabina.windahl@gmail.com>
2015-11-11 02:11:45 +0100

    Added and PASS test testToStringMultipleDiscounts()

--- src/tests/ReceiptTest.java
+++ src/tests/ReceiptTest.java
@@ -20,7 +20,7 @@ import java.util.GregorianCalendar;
 
 public class ReceiptTest {
 	Product p1 = new Product("Fejkon", 33);
-	Product p2 = new Product("Gooooodis", 22);
+	Product p2 = new Product("Godis", 22);
 	Product p3 = new Product("Avokado", 11);
 	OrderLine ol1 = new OrderLine(p1, 2);
 	OrderLine ol2 = new OrderLine(p2, 5);
@@ -58,6 +58,45 @@ public class ReceiptTest {
 	     
 		return validDList;
 	}
+	
+	private ArrayList<Discount> createDiscountListMultiple(){
+		Calendar cal;
+	    Date start;
+	    Date end;
+	    ArrayList<Discount> validDList = new ArrayList<>();
+	    cal = new GregorianCalendar();
+	    cal.set(Calendar.YEAR, 2015);
+	    cal.set(Calendar.MONTH, 10);
+	    cal.set(Calendar.DATE, 10);
+	    cal.set(Calendar.HOUR_OF_DAY, 0);
+	    cal.set(Calendar.MINUTE, 0);
+	    cal.set(Calendar.SECOND, 0);
+	    start = cal.getTime();
+	    cal.set(Calendar.YEAR, 2015);
+	    cal.set(Calendar.MONTH, 11);
+	    cal.set(Calendar.DATE, 11);
+	    cal.set(Calendar.HOUR_OF_DAY, 0);
+	    cal.set(Calendar.MINUTE, 0);
+	    cal.set(Calendar.SECOND, 0);
+	    end = cal.getTime();
+	    
+	    
+	    ArrayList<DiscountPair> discountPairList1 = new ArrayList<>();
+	    ArrayList<DiscountPair> discountPairList2 = new ArrayList<>();
+		DiscountPair discountPair1 = new DiscountPair(p1, 2);
+		DiscountPair discountPair2 = new DiscountPair(p2, 3);
+		DiscountPair discountPair3 = new DiscountPair(p3, 1);
+		discountPairList1.add(discountPair1);
+		discountPairList2.add(discountPair2);
+		discountPairList2.add(discountPair3);
+		Discount d1 = new Discount(start, end, 10.0, discountPairList1);
+		Discount d2 = new Discount(start, end, 5.0, discountPairList2);
+		validDList.add(d1);
+		validDList.add(d2);
+	     
+		return validDList;
+	}
+	
 	//Test constructors
 	@Test
 	public void testSimpleConstructor1(){
@@ -98,10 +137,26 @@ public class ReceiptTest {
 		orderLineList.add(ol3);
 		Receipt r = new Receipt(orderLineList, createDiscountList());
 		assertEquals("Fejkon : 33.0\t2 st\t66.0\n"
-				+ "Gooooodis : 22.0\t5 st\t110.0\n"
-				+ "Avokado : 11.0\t1 st\t11.0\n\n"
-				+ "Discounts: [ Tue Nov 10 00:00:00 CET 2015, Fri Dec 11 00:00:00 CET 2015, 10.0, [[ Fejkon, 2 ]] ]\n\n"
-				+ "Sum: 187.0", r.toString());
+				+ "Godis : 22.0\t5 st\t110.0\n"
+				+ "Avokado : 11.0\t1 st\t11.0\n"
+				+ "\nDiscounts: [ Tue Nov 10 00:00:00 CET 2015, Fri Dec 11 00:00:00 CET 2015, 10.0, [[ Fejkon, 2 ]] ]\n"
+				+ "\nSum: 187.0", r.toString());
+	}
+	
+	
+	
+	@Test
+	public void testToStringMultipleDiscounts(){
+		orderLineList.add(ol1);
+		orderLineList.add(ol2);
+		orderLineList.add(ol3);
+		Receipt r = new Receipt(orderLineList, createDiscountListMultiple());
+		assertEquals("Fejkon : 33.0\t2 st\t66.0\n"
+				+ "Godis : 22.0\t5 st\t110.0\n"
+				+ "Avokado : 11.0\t1 st\t11.0\n"
+				+ "\nDiscounts: [ Tue Nov 10 00:00:00 CET 2015, Fri Dec 11 00:00:00 CET 2015, 10.0, [[ Fejkon, 2 ]] ]\n"
+				+ "[ Tue Nov 10 00:00:00 CET 2015, Fri Dec 11 00:00:00 CET 2015, 5.0, [[ Godis, 3 ], [ Avokado, 1 ]] ]\n"
+				+ "\nSum: 187.0", r.toString());
 	}
 }
 
